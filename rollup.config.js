a66674d573901c6a15cc28fe4022474e01b8a8e1
Sergey Ufocoder <xufocoder@gmail.com>
2018-12-24 06:33:40 +0300

    Update `rollup.config`

--- rollup.config.js
+++ rollup.config.js
@@ -1,28 +1,15 @@
-import replace from 'rollup-plugin-replace';
-import typescript from 'rollup-plugin-typescript2';
+import typescript from "rollup-plugin-typescript2";
 import { uglify } from "rollup-plugin-uglify";
 
-const defaultEnv = {
-  ENDPOINT_HTTP: 'http://fakehost/{token}',
-  ENDPOINT_WEBSOCKET: 'ws://fakehost/{token}',
-  ENDPOINT_HEARTBEAT: 5000
-}
-
-const replaceEnv = Object.keys(defaultEnv).reduce((accumulator, key) => {
-  accumulator['process.env.' + key] = JSON.stringify(process.env[key] || defaultEnv[key])
-  return accumulator
-}, {})
-
 export default {
-  input: './tests/custom/build.test.ts',
+  input: "./lib/tracker.ts",
   output: {
-    file: './build/tracker.js',
-    format: 'umd',
-    name: '@gauf/tracker',
+    file: "./build/tracker.js",
+    format: "umd",
+    name: "@gauf/tracker",
     sourcemap: true
   },
   plugins: [
-    replace(replaceEnv),
     typescript({
       tsconfig: "./tsconfig.build.json",
     }),
