e151b354cce8e38b7c66b86a1fcad1ca1e288a07
Sergey Ufocoder <xufocoder@gmail.com>
2019-01-01 16:30:08 +0300

    Update `tokens` example

--- examples/tokens/src/tracker.js
+++ examples/tokens/src/tracker.js
@@ -3,14 +3,12 @@ import Tracker from '@gauf/tracker';
 class CustomTracker extends Tracker {
   constructor(token, settings) {
     super(
-      'http://reciever.service.com/client/{token}'.replace('{token}', token),
+      'console://reciever.service.com/client/{token}'.replace('{token}', token),
       settings
     )
   }
 }
 
-const tracker = new CustomTracker('9a08b242bc3d6e056f3b48f9dfe0f65c', {
-  packer: msgpack.encode
-})
+const tracker = new CustomTracker('9a08b242bc3d6e056f3b48f9dfe0f65c')
 
 tracker.activate({ userId: 1 })
